2dcf238222d4660b5df93560121aa14a35e258bd
Rob Meidal <rob@netapplogic.com>
2012-09-05 16:41:25 -0600

    Updated version number.

--- FalconeUtilGrailsPlugin.groovy
+++ FalconeUtilGrailsPlugin.groovy
@@ -27,7 +27,7 @@ import grails.util.GrailsUtil
 import com.infusion.util.event.EventBrokerHolder
 
 class FalconeUtilGrailsPlugin {
-  def version = "1.0"
+  def version = "1.1-DEV"
   def dependsOn = [:]
   def author = "Eric Martineau"
   def authorEmail = "ericm@infusionsoft.com"
